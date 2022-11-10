af190acb8483c79228f3df4832197ce948cd948d
Anna <khomiakova25@gmail.com>
2022-11-10 17:55:12 +0300

    feat: parsing service

--- app/services/parsing_service.rb
+++ app/services/parsing_service.rb
@@ -1,7 +1,30 @@
 require 'nokogiri'
-require 'net/http'
+require 'open-uri'
+
+BLOG  = "https://neilpatel.com/blog/voip-phone-services/"
+
 class ParsingService
-  uri  = URI("https://problogger.com/the-ultimate-guide-to-leaving-comments-on-blogs/")
-  body = Net::HTTP.get(uri)
-  document = Nokogiri::HTML(body)
+  def scrap_title
+    take_document.css('title').text
+  end
+
+  def scrap_comments
+    Nokogiri::HTML(URI.open(BLOG)).css('#comments').css('.inner').css('.comment-list')
+  end
+
+  private
+
+  def take_document
+    Nokogiri::HTML(URI.open(BLOG))
+  end
+
+  def get_blog_path(post)
+    Nokogiri::HTML(response)
+  end
+
+  def save_comments(comments, post)
+    comments.each do |comment|
+      post.comments.push(Comment.create(text: comment))
+    end
+  end
 end
