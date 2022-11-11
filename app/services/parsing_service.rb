cfbdcc21bccc002aefe76323b4d7288e710e4b52
Anna <khomiakova25@gmail.com>
2022-11-11 17:23:58 +0300

    fixed(parsing): added capybara, changed site

--- app/services/parsing_service.rb
+++ app/services/parsing_service.rb
@@ -1,7 +1,8 @@
+require 'selenium-webdriver'
 require 'nokogiri'
-require 'open-uri'
+require 'capybara'
 
-BLOG  = "https://neilpatel.com/blog/voip-phone-services/"
+BLOG  = "https://deadline.com/2022/11/netflix-sports-plans-rights-bidding-1235166831/"
 
 class ParsingService
   def scrap_title
@@ -9,22 +10,34 @@ class ParsingService
   end
 
   def scrap_comments
-    Nokogiri::HTML(URI.open(BLOG)).css('#comments').css('.inner').css('.comment-list')
+    comments_table = []
+    config_capybara
+    comments_list = take_document.css('#comment-list-wrapper')
+    if comments_list.count > 0
+      comments_list.each do |comment|
+        comments_table << comment.css('li > div > div.pmc-u-font-family-georgia > p').map(&:text)
+      end
+    end
+
   end
 
   private
 
-  def take_document
-    Nokogiri::HTML(URI.open(BLOG))
-  end
-
-  def get_blog_path(post)
-    Nokogiri::HTML(response)
-  end
-
-  def save_comments(comments, post)
-    comments.each do |comment|
-      post.comments.push(Comment.create(text: comment))
+  def config_capybara
+    Capybara.register_driver :selenium do |app|
+      Capybara::Selenium::Driver.new(app, browser: :chrome)
     end
+    Capybara.javascript_driver = :chrome
+    Capybara.configure do |config|
+      config.default_max_wait_time = 30
+      config.default_driver = :selenium
+    end
+  end
+
+  def take_document
+    browser = Capybara.current_session
+    driver = browser.driver.browser
+    browser.visit(BLOG)
+    Nokogiri::HTML(driver.page_source)
   end
 end
