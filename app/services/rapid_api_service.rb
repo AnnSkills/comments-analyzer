de14f34b1dca3a26db03b67c2419f5f7fe87cfdd
Anna <khomiakova25@gmail.com>
2022-11-15 12:31:56 +0300

    fixed(api_invoke): added hendler method  for bad status_code

--- app/services/rapid_api_service.rb
+++ app/services/rapid_api_service.rb
@@ -10,28 +10,33 @@ class RapidApiService
     @comments = comments
   end
 
+  def rapid_api_invoke
+    sentiment_request
+    http = Net::HTTP.new(URL.host, URL.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    response = http.request(@request)
+    response_json = JSON.parse(response.body)
+
+    unless response_json['status_code'] != :ok
+      raise ApiBadResponseError(response_json['status_code'])
+    end
+
+    response_json
+  end
+
+  private
+
   def sentiment_request
-    rapid_api_configuration
-    request = Net::HTTP::Post.new(url)
-    request["content-type"] = 'application/json'
-    request["X-RapidAPI-Key"] = 'dd4dd5d1e3msh12fe9a9c9343181p1052cejsn4d9543962314'
-    request["X-RapidAPI-Host"] = 'text-analysis12.p.rapidapi.com'
-    request.body = {
+    @request = Net::HTTP::Post.new(URL)
+    @request["content-type"] = 'application/json'
+    @request["X-RapidAPI-Key"] = 'dd4dd5d1e3msh12fe9a9c9343181p1052cejsn4d9543962314'
+    @request["X-RapidAPI-Host"] = 'text-analysis12.p.rapidapi.com'
+    @request.body = {
       data: {
         language: 'english',
         text: @comments
       }
     }.to_json
-    response = http.request(request)
-    response
-  end
-
-  private
-
-  def rapid_api_configuration
-    url = URI('https://text-analysis12.p.rapidapi.com/sentiment-analysis/api/v1.1')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   end
 end
