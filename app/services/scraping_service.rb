c5eba37b12dadd88c565a4a3b9fb8095b134c58b
Anna <khomiakova25@gmail.com>
2022-11-21 18:17:43 +0300

    fixed: ignore file is rewritten

--- app/services/scraping_service.rb
+++ app/services/scraping_service.rb
@@ -4,16 +4,16 @@ class ScrapingService
   end
 
   def scrape
-    @post.name = take_document.css('title').text
     comments_list = take_document.css('#comment-list-wrapper')
     if comments_list.count > 0
       comments_list.each do |comment|
-        @post.comments.text = comment.css('li > div > div.pmc-u-font-family-georgia > p').map(&:text)
-        @post.comment.post_id = @post.id
+        text = comment.css('li > div > div.pmc-u-font-family-georgia > p').map(&:text)
         sentiment = RapidApiService.new(comment).sentiment_request
-        rating = RatingService.new(sentiment['sentiment'], comment)
-        @post.comments.rating = rating.rate_comment
-        @post.rating = rating.rate_post
+        rating_service = RatingService.new(sentiment['sentiment'], comment)
+        comment_rate = rating_service.rate_comment
+        post_rate = rating_service.rate_post
+        Comment.create(text: text, post_id: @post.id, rate: comment_rate)
+        @post.update(name: take_document.css('title').text, rating: post_rate)
       end
     end
   end
