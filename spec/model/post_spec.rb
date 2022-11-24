e7512c8913350a8bce008fba8668e6071ab36041
Anna <khomiakova25@gmail.com>
2022-11-24 16:40:36 +0300

    fixed: model test, yml, spec dependence

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
-RSpec.descripe Post, type: :model do
+describe Post do
+  before(:each) do
+    @attributes = {
+      :name => 'Testing post',
+      :url => 'https://deadline.com/2022/11/death-in-paradise-our-house-itv-louise-candlish-red-planet-tony-jordan-1235162390'
+    }
+  end
+
+  it "should create new post with given valid attributes" do
+    Post.create!(@attributes)
+  end
+
   it 'post should have url' do
-    expect(Post.new(name: 'Testing post')).to_not be_valid
+    expect(Post.new(@attributes.merge(:url => ''))).to be_valid
   end
 
   it 'post should have name' do
-    expect(Post.new(url: 'https://deadline.com/2022/11/death-in-paradise-our-house-itv-louise-candlish-red-planet-tony-jordan-1235162390')).to_not be_valid
+    expect(Post.new(@attributes.merge(:name => ''))).to be_valid
+  end
+
+  it "should reject duplicate post url" do
+    Post.create!(@attributes)
+    user_with_duplicate_email = Post.new(@attributes)
+    expect(user_with_duplicate_email).to be_valid
   end
 
   it 'is not valid if the data is not filled' do
@@ -14,12 +31,9 @@ RSpec.descripe Post, type: :model do
     expect(Post.count).to eq 1
   end
 
-  it { should validate_uniqueness_of(:url).on(:create)  }
-
   it 'is comment added to post' do
     post_test = Post.create(url: 'https://deadline.com/2022/11/death-in-paradise-our-house-itv-louise-candlish-red-planet-tony-jordan-1235162390', name: 'Post name test')
-    Comment.create(text: 'Comment one for test post', post_id: post.id)
-
+    Comment.create(text: 'Comment one for test post', post_id: post_test.id)
     expect(post_test.comments.size).to eq 1
   end
 end
