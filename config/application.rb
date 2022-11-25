ce8ed599bf8cbd96658a0f3fa34e7f357c0eb4d5
Anna <khomiakova25@gmail.com>
2022-11-25 14:02:38 +0300

    fix: add config eager load path for services

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Onliner
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load = true
+    config.eager_load_paths += %W(#{config.root}/app/lib)
+    config.eager_load_paths += %W(#{config.root}/app/services)
   end
 end
