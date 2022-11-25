eb6d375657617330e57016691d3ec2885cb08dd8
Anna <khomiakova25@gmail.com>
2022-11-25 12:05:53 +0300

    fixed: application.rb configuration

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Onliner
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load = true
+    config.eager_load_paths += %W(#{config.root}/app/lib)
   end
 end
